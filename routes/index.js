de9eea7126887c310434b01c65dadab95a5c57aa
Gideon Arces <gid.arces07@gmail.com>
2020-10-14 05:45:04 +0800

    Deleting unused variables

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require("express");
-const databaseAccess = require("../utils/databaseAccess");
 var router = express.Router();
-var database = require("../utils/databaseAccess");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
